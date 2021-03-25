5fc8a0575f71a2f8651976ba2f3a77cb6ef05430
Luming Tang <happyallen24@gmail.com>
2021-03-25 00:55:41 -0400

    Update CUB wget
    the previous CUB data link doesn't work anymore. the data has been moved to a good drive folder.

--- dataset/download.sh
+++ dataset/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "downloading CUB..."
-wget http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1hbzc_P1FuxMkcabkgn9ZKinBwW683j45' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1hbzc_P1FuxMkcabkgn9ZKinBwW683j45" -O CUB_200_2011.tgz && rm -rf /tmp/cookies.txt
 tar -xzf CUB_200_2011.tgz
 rm CUB_200_2011.tgz
 rm attributes.txt
